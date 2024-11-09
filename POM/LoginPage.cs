4e6e05b7af1f505a492c846d4e2ed0616efb27de
Devarla Bhimesh <devarla_bhimesh@epam.com>
2024-11-09 18:53:52 +0530

    Locators created

---
+++ POM/LoginPage.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+
+namespace POM
+{
+    public class LoginPage
+    {
+        private By username = By.XPath("//input[@name='user-name']");
+        private By password = By.XPath("//input[@name='password']");
+        private By loginButton = By.XPath("//input[@name='login-button']");
+    }
+}
