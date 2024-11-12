fb4f3c71f1308d7c32766ce79fc1c91382373ded
Devarla Bhimesh <91611346+bhimesh01@users.noreply.github.com>
2024-11-12 11:24:46 +0530

    Update POM/LoginPage.cs

--- POM/LoginPage.cs
+++ POM/LoginPage.cs
@@ -7,5 +7,30 @@ namespace POM
         private By username = By.XPath("//input[@name='user-name']");
         private By password = By.XPath("//input[@name='password']");
         private By loginButton = By.XPath("//input[@name='login-button']");
+
+        public void NavigateToLoginPage()
+        {
+            // Code to navigate to the login page
+        }
+
+        public void EnterValidCredentials()
+        {
+            // Code to enter valid credentials and click the login button
+        }
+
+        public void EnterInvalidCredentials()
+        {
+            // Code to enter invalid credentials and click the login button
+        }
+
+        public void CheckHomePageRedirection()
+        {
+            // Code to check if the user is redirected to the home page
+        }
+
+        public void CheckErrorMessageDisplay()
+        {
+            // Code to check if an error message is displayed
+        }
     }
 }
